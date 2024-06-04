6f02e35b587e4f80c099cb47c989bd8cdf02dcaf
rmairena97 <rmairena@mintrabajo.gob.gt>
2024-06-04 17:42:41 -0600

    Adding controllers

---
+++ app/Http/Controllers/OccupationPersonController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OccupationPersonController extends Controller
+{
+    //
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\DisabledPersonController;
 use App\Http\Controllers\PersonController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(PersonController::class)->group(function () {
     Route::post('/getPersonById', 'getPersonById');
     Route::post('/store', 'store');
+});
+
+Route::controller(DisabledPersonController::class)->group(function () {
+    Route::post('/setDisability', 'setDisability');
 });
